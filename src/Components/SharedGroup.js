a8eb8e32e357165b19be67272d4dff2317df4ec6
shawn-goh24 <ahshawngoh@gmail.com>
2023-03-20 23:14:08 +0800

    allow permission to enter page based on invited emails/members

--- src/Components/SharedGroup.js
+++ src/Components/SharedGroup.js
@@ -13,13 +13,13 @@ import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
 import AddIcon from "@mui/icons-material/Add";
 import { database } from "../firebase";
-import { onValue, ref, set } from "firebase/database";
+import { onValue, ref, runTransaction, set } from "firebase/database";
 import Fade from "@mui/material/Fade";
 import Paper from "@mui/material/Paper";
 import Popper from "@mui/material/Popper";
 import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
-
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import {
   Dialog,
   DialogContent,
@@ -31,6 +31,9 @@ import {
   FormControl,
   FilledInput,
   Button,
+  Tooltip,
+  DialogActions,
+  Link,
 } from "@mui/material";
 
 const drawerWidth = 240;
@@ -40,28 +43,25 @@ export default function SharedGroup(props) {
   const [input, setInput] = useState("");
   const [invited, setInvited] = useState([]);
   const [copied, setCopied] = useState("Copy link");
-
   const [shared, setShared] = useState([]);
-  const [imgUrl, setImgUrl] = useState([]);
-
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [open, setOpen] = useState(true);
-  const [placement, setPlacement] = useState();
+  const [open, setOpen] = useState(false);
+  const [state, setState] = useState("invite");
+  const [remove, setRemove] = useState([]);
 
   const { tripId } = props;
 
-  // useEffect(() => {
-  //   const avatarRef = ref(database, `trips/${tripId}`);
-  //   onValue(avatarRef, (data) => {
-  //     const tmp = [data.val().creatorId];
-  //     setShared([...tmp, ...Object.keys(data.val().sharedWith)]);
-  //   });
+  useEffect(() => {
+    const avatarRef = ref(database, `trips/${tripId}`);
+    onValue(avatarRef, (data) => {
+      const tmp = [data.val().creatorId];
+      setShared([...tmp, ...Object.keys(data.val().members)]);
+    });
 
-  //   const userAvatarRef = ref(database, "users");
-  //   onValue(userAvatarRef, (url) => {
-  //     console.log(Object.values(url.val()));
-  //   });
-  // }, []);
+    // const userAvatarRef = ref(database, "users");
+    // onValue(userAvatarRef, (url) => {
+    //   console.log(Object.values(url.val()));
+    // });
+  }, []);
 
   const drawer = (
     <Box>
@@ -137,10 +137,124 @@ export default function SharedGroup(props) {
     setInvited([...tmp2, ...tmp]);
   };
 
+  const members = shared.map((email) => {
+    const tmp = email;
+    const newEmail = tmp.replace("*", ".");
+    return (
+      <Box
+        id={newEmail}
+        key={newEmail}
+        sx={{
+          border: "1px solid black",
+          p: "0px 5px",
+          m: "2px",
+          display: "flex",
+          width: "fit-content",
+          backgroundColor: "#f2f2f2",
+          borderRadius: "20px",
+          alignItems: "center",
+        }}
+      >
+        <Typography>{newEmail}</Typography>
+        <IconButton onClick={() => selectMember(newEmail)}>
+          <CloseIcon sx={{ width: "15px", height: "15px" }} />
+        </IconButton>
+      </Box>
+    );
+  });
+
+  const selectMember = (email) => {
+    const currColor = document.getElementById(email).style.backgroundColor;
+    console.log(document.getElementById(email).style.backgroundColor);
+    console.log(currColor);
+    document.getElementById(email).style.backgroundColor ===
+      "rgb(242, 242, 242)" ||
+    document.getElementById(email).style.backgroundColor === ""
+      ? (document.getElementById(email).style.backgroundColor =
+          "rgb(83, 115, 94)")
+      : (document.getElementById(email).style.backgroundColor =
+          "rgb(242, 242, 242)");
+
+    if (!remove.includes(email)) {
+      const tmp = [email];
+      setRemove([...remove, ...tmp]);
+    } else {
+      const tmp2 = remove;
+      const index = tmp2.indexOf(email);
+      tmp2.splice(index, 1);
+      const tmp3 = [];
+      setRemove([...tmp3, ...tmp2]);
+    }
+  };
+
+  const removeMember = () => {
+    const shareRef = ref(database, `trips/${tripId}`);
+    runTransaction(shareRef, (data) => {
+      console.log(data);
+      for (let i = 0; i < remove.length; i++) {
+        const tmp = remove[i];
+        const ans = tmp.replace(".", "*");
+        if (data) {
+          if (data.members && data.members[ans]) {
+            data.members[ans] = null;
+          } else {
+            continue;
+          }
+        }
+      }
+      return data;
+    });
+
+    setRemove([]);
+    setOpen(!open);
+    setState("invite");
+  };
+
   const copy = async () => {
     await navigator.clipboard.writeText(link);
     setCopied("Copied");
   };
+
+  const sendInvite = () => {
+    const shareRef = ref(database, `trips/${tripId}`);
+    runTransaction(shareRef, (data) => {
+      console.log(data);
+      for (let i = 0; i < invited.length; i++) {
+        const tmp = invited[i];
+        const ans = tmp.replace(".", "*");
+        if (data) {
+          if (data.members && data.members[ans]) {
+            continue;
+          } else {
+            if (!data.members) {
+              data.members = {};
+            }
+            data.members[ans] = true;
+          }
+        }
+      }
+      return data;
+    });
+
+    setInvited([]);
+    setOpen(!open);
+  };
+
+  const avatars = shared.map((email) => {
+    return <Avatar key={email} alt={email} />;
+  });
+
+  const tooltip = shared.map((user) => {
+    const tmpUser = user.replace("*", ".");
+    return (
+      <div key={tmpUser}>
+        {tmpUser}
+        <br />
+      </div>
+    );
+  });
+
+  // console.log(remove);
 
   // To get current link
   // console.log(window.location.href + "/shared");  // add share to those that want to view only
@@ -170,69 +284,111 @@ export default function SharedGroup(props) {
         <Box>{drawer}</Box>
 
         <Box sx={{ display: "flex", justifyContent: "flex-end", pr: 2, py: 2 }}>
-          <AvatarGroup max={4}>
-            {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-            <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" /> */}
-          </AvatarGroup>
+          <Tooltip title={tooltip} arrow>
+            <AvatarGroup max={4}>{avatars}</AvatarGroup>
+          </Tooltip>
           <Avatar onClick={() => setOpen(!open)}>
             <AddIcon />
           </Avatar>
           <Dialog open={open} maxWidth="xs">
-            <DialogTitle sx={{ display: "flex" }}>
-              <Typography>Invite friends</Typography>
-              <IconButton
-                onClick={() => {
-                  setOpen(!open);
-                  setCopied("copy link");
-                }}
-              >
-                <CloseIcon sx={{ color: "#CCCCCC" }} />
-              </IconButton>
-            </DialogTitle>
-            <DialogContent>
-              <Box mb={2}>
-                <TextField size="small" disabled value={link} />
-                <Button onClick={copy}>{copied}</Button>
-              </Box>
-              <Box component="form" onSubmit={handleSubmit} width="100%">
-                {/* <FormControl variant="filled">
-                  <FilledInput
-                    required
-                    id="component-filled"
-                    type="email"
-                    TextField
-                    size="small"
-                    onChange={(e) => setValue(e.target.value)}
-                  />
-                </FormControl> */}
-                <TextField
-                  required
-                  value={input}
-                  label="Invite by email"
-                  size="small"
-                  type="email"
-                  onChange={(e) => setInput(e.target.value)}
-                />
-                <Button type="submit">Add</Button>
-              </Box>
-              {invited.length !== 0 ? (
-                <>
-                  <Box sx={{ display: "flex", flexWrap: "wrap" }}>{emails}</Box>
-                  <TextField multiline rows={6} fullWidth />
-                  <Button onClick={() => console.log("send invite")}>
-                    Send invite
+            {state === "invite" ? (
+              <>
+                <DialogTitle
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography sx={{ fontSize: "28px" }}>
+                    Invite friends
+                  </Typography>
+                  <IconButton
+                    onClick={() => {
+                      setOpen(!open);
+                      setCopied("copy link");
+                      setState("invite");
+                    }}
+                  >
+                    <CloseIcon sx={{ color: "#CCCCCC" }} />
+                  </IconButton>
+                </DialogTitle>
+                <DialogContent dividers>
+                  <Box mb={2}>
+                    <TextField size="small" disabled value={link} />
+                    <Button onClick={copy}>{copied}</Button>
+                  </Box>
+                  <Box component="form" onSubmit={handleSubmit} width="100%">
+                    <TextField
+                      fullWidth
+                      required
+                      value={input}
+                      label="Invite by email"
+                      size="small"
+                      type="email"
+                      onChange={(e) => setInput(e.target.value)}
+                    />
+                  </Box>
+                  {invited.length !== 0 ? (
+                    <>
+                      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+                        {emails}
+                      </Box>
+                      {/* <TextField multiline rows={6} fullWidth /> */}
+                      <Button onClick={() => sendInvite()}>Send invite</Button>
+                    </>
+                  ) : (
+                    ""
+                  )}
+                  <br />
+                </DialogContent>
+                <DialogActions
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Link
+                    sx={{ cursor: "pointer" }}
+                    underline="hover"
+                    onClick={() => setState("manage members")}
+                  >
+                    Manage trip members
+                  </Link>
+                </DialogActions>
+              </>
+            ) : (
+              <>
+                <DialogTitle
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Box sx={{ display: "flex" }}>
+                    <IconButton onClick={() => setState("invite")}>
+                      <ArrowBackIosIcon />
+                    </IconButton>
+                    <Typography sx={{ fontSize: "28px" }}>
+                      Manage trip members
+                    </Typography>
+                  </Box>
+                  <IconButton
+                    onClick={() => {
+                      setOpen(!open);
+                      setCopied("copy link");
+                      setState("invite");
+                    }}
+                  >
+                    <CloseIcon sx={{ color: "#CCCCCC" }} />
+                  </IconButton>
+                </DialogTitle>
+                <DialogContent dividers>
+                  <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+                    {members}
+                  </Box>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    className="btn-green"
+                    sx={{ color: "white" }}
+                    onClick={() => removeMember()}
+                  >
+                    Remove member
                   </Button>
-                </>
-              ) : (
-                ""
-              )}
-              <br />
-              <Divider />
-              <Box>Manage trip members</Box>
-            </DialogContent>
+                </DialogActions>
+              </>
+            )}
           </Dialog>
         </Box>
       </Drawer>
